049fa71b83e69bcc22c57c34d1168cc91c06fe67
尤晓翔 <youxx@mxchip.com>
2017-11-13 11:23:21 +0800

    更改没有更改的配网方式

--- filesystem/filesystem_fatfs/mico_config.h
+++ filesystem/filesystem_fatfs/mico_config.h
@@ -53,7 +53,7 @@
  * Enable wlan connection, start easylink configuration if no wlan settings are existed */
 //#define MICO_WLAN_CONNECTION_ENABLE
 
-#define MICO_CONFIG_MODE CONFIG_MODE_EASYLINK_WITH_SOFTAP
+#define MICO_CONFIG_MODE CONFIG_MODE_EASYLINK_AWS
 
 #define EasyLink_TimeOut                60000 /**< EasyLink timeout 60 seconds. */
 
